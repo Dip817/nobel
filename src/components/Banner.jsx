4e37371ddfbede1d6d8a84a2933444f05c901bb4
Diptanshu Dey <diptanshudey4@gmail.com>
2024-12-20 14:35:18 +0530

    nobel banner completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import '../assets/css/banner.css'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/imgs/logo-nobel.png'
 function Banner() {
     return (
         <>
-            <div className='banner-main d-flex flex-column border'>
-               <div className='d-flex flex-column' style={{alignItems:'center'}}>
-               <h2>Your Child's Future Begins Here</h2>
-                <p>Discover the best kid's school</p>
-                </div> 
+            <div className='banner-main d-flex flex-column border h-100'>
+                <div className='logo'>
+                    <img src={Logo} alt='' />
+                </div>
+                <div className='d-flex flex-column  w-100' style={{ alignItems: 'center', marginTop: '10%' }}>
+                    <h2>Your Child's Future Begins Here</h2>
+                    <p style={{fontWeight:'200'}}>Discover the best kid's school</p>
+                </div>
                 <div className='banner-buttons d-flex'>
-                    <button>Admissions</button>
-                    <Link to=''>Learn More</Link>
+                    <button className='fill-btn'>Admissions</button>
+                    <Link className='hollow-btn' to=''>Learn More</Link>
+                </div>
+                <div className='twisted-line'>
+                    <p>Secure your child's future</p>
                 </div>
             </div>
 
