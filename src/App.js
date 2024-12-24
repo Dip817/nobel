3c752e5e325e176e513afc3094672a3ea778fe41
Diptanshu Dey <diptanshudey4@gmail.com>
2024-12-24 13:25:32 +0530

    nobel contactform

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Contactform from './components/Contactform';
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import AboutComponent from './components/AboutComponent'
-import Principles from './components/Principles'
-import ProgrammeComp from './components/ProgrammeComp'
+import Contactform from './components/Contactform'
 function App() {
-  const router = createBrowserRouter([
-    {
-      path:'/',
-      element:<>
-      <Navbar/>
-      <Banner/>
-      <AboutComponent/>
-      <Principles/>
-      <ProgrammeComp/>
-        <Contactform/>       
-      </>
-    },    
+  const router = createBrowserRouter([      
     {
       path:'/contact',
       element:<>
--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../assets/css/contactform.css'
-function contactform() {
+function Contactform() {
   return (
     <>
-    <div className='contactform-main d-flex mt-5'>
+    <div className='contactform-main d-flex flex-wrap mt-5'>
         <div className='contactform-left'>
-        <div class='contact-info'>
+        <div class='contact-info d-flex flex-column'>
             <h1>Contact us</h1>
-            <p>register for daily updates</p>
+            <p style={{fontSize:'20px',fontWeight:'400px'}}>Register for daily updates</p>
             <div class="details">
                 <p><b>&#x2709; </b> hello@relume.io</p>
                 <p><i class='bx bxs-phone'></i> +1 (555) 000-0000</p>
@@ -16,23 +17,25 @@ function contactform() {
         </div>
         </div>
         <div div className='contactform-right'>
-        <form>
+        <form class="contact-form">
+            <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="Your name" required></input>
-
+                <input type="text" id="name" name="name" placeholder="Your Name" required></input>
+            </div>
+            <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Your email" required></input>
-
+                <input type="email" id="email" name="email" placeholder="Your Email" required></input>
+            </div>
+            <div class="form-group">
                 <label for="message">Message</label>
-                <textarea id="message" name="message" rows="4" placeholder="Type your message..." required></textarea>
-
-                <div class="checkbox-container">
-                    <input type="checkbox" id="terms" name="terms" required></input>
-                    <label for="terms">I accept the <a href="/">Terms</a></label>
-                </div>
-
-                <button type="submit">Submit</button>
-            </form>
+                <textarea id="message" name="message" placeholder="Type your message..." rows="5" cols="4" required></textarea>
+            </div>
+            <div class="form-group terms">
+                <input type="checkbox" id="terms" name="terms" required></input>
+                <label for="terms">I accept the <Link to=''>Terms</Link></label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
         </div>
                
     </div>
@@ -40,4 +43,4 @@ function contactform() {
   )
 }
 
-export default contactform
+export default Contactform
