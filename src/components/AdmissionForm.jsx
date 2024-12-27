abe678a9c6f39c8a63c31b03a86b0a4167533514
Diptanshu Dey <diptanshudey4@gmail.com>
2024-12-27 17:10:05 +0530

    nobel admissionform

--- src/components/AdmissionForm.jsx
+++ src/components/AdmissionForm.jsx
@@ -4,12 +4,164 @@ function AdmissionForm() {
   return (
     <>
     <div className='admissionform-main'>
-    <div className='admissionform-heading'>
-        <h1>Application Form For Admission</h1>
+    <div class="form-container">
+        <h1>Admission At Vancouver International School</h1>
+        <form>
+        {/* first div */}
+        <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="branch">Please Select Branch</label>
+                <select id="branch" name="branch">
+                    <option>KPHP</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="reg-number">Registration Number*</label>
+                <input type="text" id="reg-number" name="reg-number"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="photo">Photo of the Student</label>
+                <input type="file" id="photo" name="photo"></input>
+            </div>
+            </div>
+            {/* first div ends here */}
+
+            {/* second div starts from here */}
+            <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="first-name">First Name</label>
+                <input type="text" id="first-name" name="first-name"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="middle-name">Middle Name</label>
+                <input type="text" id="middle-name" name="middle-name"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="last-name">Last Name</label>
+                <input type="text" id="last-name" name="last-name"></input>
+            </div>
+            </div>
+            <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="gender">Gender</label>
+                <select id="gender" name="gender">
+                    <option>Male</option>
+                    <option>Female</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="dob">Date of Birth</label>
+                <input type="date" id="dob" name="dob"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="class">Admission To Class</label>
+                <select id="class" name="class">
+                    <option>4th</option>
+                </select>
+            </div>
+            </div>
+            {/* second div ends here */}
+
+            {/* third div starts from here */}
+            <h2>Parent/Guardian</h2>
+            <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="relation">Choose Relation</label>
+                <select id="relation" name="relation">
+                    <option>Father</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="father-first-name">Father First Name</label>
+                <input type="text" id="father-first-name" name="father-first-name"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="father-last-name">Father Last Name</label>
+                <input type="text" id="father-last-name" name="father-last-name"></input>
+            </div>
+            </div>
+            <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="father-email">Email Address</label>
+                <input type="email" id="father-email" name="father-email" placeholder="Example@gmail.com"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="father-phone">Phone Number</label>
+                <input type="tel" id="father-phone" name="father-phone"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="father-qualification">Education Qualification</label>
+                <select id="father-qualification" name="father-qualification">
+                    <option>B.Tech</option>
+                </select>
+            </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="father-address">Residential Address of the Parent/Guardian</label>
+                <textarea id="father-address" name="father-address" placeholder="Please type your address here..."></textarea>
+            </div>
+            {/* third div ends here */}
+
+            {/* fourth div starts from here */}
+            <h2>Parent/Guardian</h2>
+            <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="mother-relation">Choose Relation</label>
+                <select id="mother-relation" name="mother-relation">
+                    <option>Mother</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="mother-first-name">Mother First Name</label>
+                <input type="text" id="mother-first-name" name="mother-first-name"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="mother-last-name">Mother Last Name</label>
+                <input type="text" id="mother-last-name" name="mother-last-name"></input>
+            </div>
+            </div>
+            <div className='d-flex w-100 justify-content-between'>
+            <div class="form-group">
+                <label for="mother-email">Email Address</label>
+                <input type="email" id="mother-email" name="mother-email" placeholder="Example@gmail.com"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="mother-phone">Phone Number</label>
+                <input type="tel" id="mother-phone" name="mother-phone"></input>
+            </div>
+
+            <div class="form-group">
+                <label for="mother-qualification">Education Qualification</label>
+                <select id="mother-qualification" name="mother-qualification">
+                    <option>B.Tech</option>
+                </select>
+            </div>
+            </div>
+            <div class="form-group">
+                <label for="mother-address">Residential Address of the Parent/Guardian</label>
+                <textarea id="mother-address" name="mother-address" placeholder="Please type your address here..."></textarea>
+            </div>
+            {/* fourth div ends here */}
+
+            <button type="submit" class="btn">Apply Now 
+            </button>
+        </form>
     </div>
     </div>
     </>
   )
 }
-
 export default AdmissionForm
